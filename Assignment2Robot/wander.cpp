28acf87e162efb5d1ff43aecd182dced7fabc289
Safrapath <P15188966@my365.dmu.ac.uk>
2018-02-08 19:35:58 +0000

    Made more improvements to Wander

--- Assignment2Robot/wander.cpp
+++ Assignment2Robot/wander.cpp
@@ -4,7 +4,7 @@
 #include "wander.h"
 
 wander::wander() : ArAction("Wander around") {
-	speed = 400;
+	speed = 200;
 	heading = 8;
 	state = forwards;
 	oldX = 0;
@@ -17,8 +17,6 @@ ArActionDesired * wander::fire(ArActionDesired d) {
 	desiredState.reset();
 
 	travelled = sqrt(pow(myRobot->getX() - oldX, 2) + pow(myRobot->getY() - oldY, 2));
-
-	//printf("angle = %.2f, trav = %.2f dist = %.2f\n", angle, travelled, distance);
 
 	currentAngle = myRobot->getTh();
 	aim = oldAngle + angle;
@@ -36,28 +34,36 @@ ArActionDesired * wander::fire(ArActionDesired d) {
 		speed = 200;
 		break;
 	case turn:
-		printf("currentAngle = %.2f, angle = %.2f, trav = %.2f dist = %.2f\n", currentAngle, angle, travelled, distance);
+		//printf("currentAngle = %.2f, angle = %.2f\n", currentAngle, angle);
 
-		if (!(currentAngle >= aim - 5 && currentAngle <= aim + 5)) {
-			speed = 25;
-			desiredState.setDeltaHeading(heading);
-		}
-		else {
+		if (currentAngle >= aim - 5 && currentAngle <= aim + 5) {
 			oldX = myRobot->getX();
 			oldY = myRobot->getY();
 			oldAngle = myRobot->getTh();
 			state = forwards;
-			distance = rand() % 1000 + 500;
+			distance = (rand() % 1000 + 500);
 
-			angle = rand() % 140 + -140;
+			/*int min = -90;
+			int max = 180;
+			angle = (min + (rand() % (int)(max - min)));*/
+
+
+			angle = rand() % 280 + -140;
+
+			heading = angle;
 
 			travelled = 0;
 
-			if (angle <= 0) {
+			if (angle <= currentAngle) {
 				heading = -heading;
 			}
 
+
 			printf("wander: angle %.2f     distance %.2f\n", angle, distance);
+		}
+		else {
+			speed = 50;
+			desiredState.setDeltaHeading(heading);
 		}
 		break;
 
